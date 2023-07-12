0b6041798ecb86296ae486bef222cb9d5c46a509
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-07-12 11:30:17 -0700

    Fix styling and add reviews list

---
+++ reviews.js
@@ -0,0 +1,24 @@
+export const reviews = [
+    {
+        id: 0,
+        image: 'images/cat.jpg',
+        name: 'John Doe',
+        title: 'CEO',
+        description: 'The best ever!'
+    },
+    {
+        id: 1,
+        image: 'images/cat.jpg',
+        name: 'Jane Doe',
+        title: 'COO',
+        description: 'The second best ever!'
+    },
+    {
+        id: 2,
+        image: 'images/cat.jpg',
+        name: 'Your cat',
+        title: 'Professional cat',
+        description: 'Amazing!'
+    }
+];
+
