a251292f7e3e54384bb93a526fef32583aed7b62
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-07-12 11:42:11 -0700

    Implement carousel logic (unfinished)

--- carousel.js
+++ carousel.js
@@ -2,3 +2,15 @@ import { reviews } from 'highlight.js';
 
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
+
+let i = 0;
+
+nextBtn.addEventListener('click', function() {
+    i++;
+    document.getElementById('review-name').innerHTML = reviews[i]['name'];
+});
+
+prevBtn.addEventListener('click', function() {
+    i--;
+    document.getElementById('review-name').innerHTML = reviews[i]['name'];
+});
