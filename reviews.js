7756ea32036118d48598da29775d41500428868e
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-07-14 10:39:08 -0700

    Add review

--- reviews.js
+++ reviews.js
@@ -1,23 +1,26 @@
 export const reviews = [
     {
-        id: 0,
         image: 'images/johndoe.jpg',
         name: 'John Doe',
         title: 'Co-Founder',
         description: 'The best ever!'
     },
     {
-        id: 1,
         image: 'images/janedoe.jpeg',
         name: 'Jane Doe',
         title: 'Co-Founder',
         description: 'My favorite!'
     },
     {
-        id: 2,
         image: 'images/cat.jpg',
         name: 'Your cat',
         title: 'Professional cat',
         description: 'Amazing!'
+    },
+    {
+        image: 'images/keyeboard.jpg',
+        name: 'Keye Board',
+        title: 'Customer',
+        description: 'I\'ve been with this company for years, and they have never failed me!'
     }
 ];
